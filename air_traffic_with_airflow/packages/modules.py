80824a40519570961705e8c77f11b932fc1995e0
Wojciech Judt <wojciech.judt@gmail.com>
2022-08-22 19:34:46 +0200

    clearing first half of data

--- air_traffic_with_airflow/packages/modules.py
+++ air_traffic_with_airflow/packages/modules.py
@@ -1,6 +1,7 @@
 import datetime
 import pandas as pd
 
+
 def get_date():
     current_time = datetime.datetime.now()
     past_time = current_time - datetime.timedelta(hours=1)
@@ -10,12 +11,42 @@ def get_date():
     print(f'current time: {unix_current_timestamp}')
 
 
-def clean_data():
-    df = pd.read_csv("zone1_20220822T124500.txt", delimiter=',', header=None)
-    df = df.drop(columns=[8, 12, 15, 16, 17], axis=1)
-    # df.columns = ['']
+def unix_time_to_datetime(unix_time):
+    dt = datetime.datetime.fromtimestamp(unix_time)
+    return dt
+
+
+def clean_data(path):
+    df = pd.read_csv(path, delimiter=',', header=None)
+    df = df.drop(columns=17, axis=1)
+    df = df.drop(df.tail(1).index)
+    df.columns = ['icao24',
+                  'callsign',
+                  'origin_country',
+                  'time_position',
+                  'time_last_contact',
+                  'longitude_deg',
+                  'latitude_deg',
+                  'geo_altitude_m',
+                  'on_ground',
+                  'velocity_m_per_s',
+                  'true_track_dec_deg',
+                  'vertical_rate_m_per_s',
+                  'sensors',
+                  'baro_altitude_m',
+                  'squawk_code',
+                  'spi',
+                  'position_source']
+
+    df['icao24'] = df['icao24'].str.split('[').str[-1].str.replace("'", "").str.strip()
+    df['callsign'] = df['callsign'].str.replace("'", "").str.strip()
+    df['origin_country'] = df['origin_country'].str.replace("'", "").str.strip()
+    df['time_position'] = df['time_position'].apply(unix_time_to_datetime)
+    df['time_last_contact'] = df['time_last_contact'].apply(unix_time_to_datetime)
+
+    print(df.dtypes)
     return df
 
 
 if __name__ == "__main__":
-    clean_data()
+    clean_data("zone1_20220822T124500.txt")
