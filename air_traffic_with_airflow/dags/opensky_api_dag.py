b1e980ec9ea3964ecb68db31394c76825acd0269
Wojciech Judt <wojciech.judt@gmail.com>
2022-08-22 17:45:36 +0200

    creating empty postgres database in a docker file and minio bucket; also a first task in a pipeline is finished (responsible for sending .csv file into bucket from data downloaded from opensky_api

--- air_traffic_with_airflow/dags/opensky_api_dag.py
+++ air_traffic_with_airflow/dags/opensky_api_dag.py
@@ -1,8 +1,12 @@
-from airflow import DAG
 from datetime import datetime, timedelta
-from airflow.operators.python import PythonOperator
+
 from opensky_api_master.python.opensky_api import OpenSkyApi
 from constant_values import api_login_data
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+
 
 default_args = {
     'owner': 'wjudt',
@@ -11,24 +15,35 @@ default_args = {
 }
 
 
-def get_api_data(username, password):
+def get_api_data_sent_to_minio(username, password, ts_nodash):
     api = OpenSkyApi(username=username, password=password)
-    s = api.get_states(bbox=(51, 53, 16, 18))
-    print(s)
+    response = api.get_states(bbox=(51, 53, 16, 18))
+    data = str(response)
+
+    s3_hook = S3Hook(aws_conn_id="minio_connection")
+    s3_hook.load_string(
+        string_data=data,
+        key=f"zone1_{ts_nodash}.csv",
+        bucket_name="open-sky-raw-data",
+        replace=True
+    )
+
+    print(data)
+    print(type(data))
 
 
 with DAG(
         default_args=default_args,
         dag_id='open_sky_api',
         description='Dag for retrieving data from open sky api',
-        start_date=datetime(2022, 8, 20),
+        start_date=datetime(2022, 8, 22),
         schedule_interval='*/15 * * * *',
         catchup=False,
         tags=['open_sky']
 ) as dag:
     task1 = PythonOperator(
         task_id='get_api_data_sent_raw_to_minio',
-        python_callable=get_api_data,
+        python_callable=get_api_data_sent_to_minio,
         op_kwargs={'username': api_login_data.api_username,
                    'password': api_login_data.api_password}
     )
--- air_traffic_with_airflow/packages/modules.py
+++ air_traffic_with_airflow/packages/modules.py
@@ -1,8 +1,21 @@
 import datetime
+import pandas as pd
 
-current_time = datetime.datetime.now()
-past_time = current_time - datetime.timedelta(hours=1)
-unix_current_timestamp = round(datetime.datetime.timestamp(current_time))
-unix_past_timestamp = round(datetime.datetime.timestamp(past_time))
-print(f'past time: {unix_past_timestamp}')
-print(f'current time: {unix_current_timestamp}')
+def get_date():
+    current_time = datetime.datetime.now()
+    past_time = current_time - datetime.timedelta(hours=1)
+    unix_current_timestamp = round(datetime.datetime.timestamp(current_time))
+    unix_past_timestamp = round(datetime.datetime.timestamp(past_time))
+    print(f'past time: {unix_past_timestamp}')
+    print(f'current time: {unix_current_timestamp}')
+
+
+def clean_data():
+    df = pd.read_csv("zone1_20220822T124500.txt", delimiter=',', header=None)
+    df = df.drop(columns=[8, 12, 15, 16, 17], axis=1)
+    # df.columns = ['']
+    return df
+
+
+if __name__ == "__main__":
+    clean_data()
