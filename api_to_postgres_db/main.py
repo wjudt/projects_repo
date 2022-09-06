dd119386f0e617a4425cbbe3e9e1c27213290de3
Wojciech Judt <wojciech.judt@gmail.com>
2022-09-06 13:22:37 +0200

    adding to api two new get requests read_flight_by_icao and read flights_by_date

--- api_to_postgres_db/main.py
+++ api_to_postgres_db/main.py
@@ -1,3 +1,5 @@
+import datetime
+
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
@@ -22,3 +24,19 @@ def read_flights(skip: int = 0, limit: int = 100, db: Session = Depends(get_db))
     return flights
 
 
+@app.get("/flight/icao24={icao}", response_model=schemas.FlightBase)
+def read_flight_by_icao(icao: str, db: Session = Depends(get_db)):
+    flight_by_icao = read.get_flight_by_icao(db, icao=icao.lower())
+    if flight_by_icao is None:
+        raise HTTPException(status_code=404, detail="Flight not found")
+    return flight_by_icao
+
+
+@app.get("/flights/startdate={start}enddate={end}", response_model=list[schemas.FlightBase])
+def read_flights_by_date(start: str, end: str, db: Session = Depends(get_db)):
+    flights_by_date = read.get_flights_by_date(db,
+                                               start_date=datetime.datetime.strptime(start, "%Y-%m-%dT%H:%M:%S"),
+                                               end_date=datetime.datetime.strptime(end, "%Y-%m-%dT%H:%M:%S"))
+    if flights_by_date is None:
+        raise HTTPException(status_code=404, detail="Flight not found")
+    return flights_by_date
--- api_to_postgres_db/read.py
+++ api_to_postgres_db/read.py
@@ -1,6 +1,21 @@
+import datetime
+
 from sqlalchemy.orm import Session
+from sqlalchemy import and_
 import models
 
 
 def get_flights(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Flight).offset(skip).limit(limit).all()
+
+
+def get_flight_by_icao(db: Session, icao: str):
+    return db.query(models.Flight).filter(models.Flight.icao24 == icao).first()
+
+
+def get_flights_by_date(db: Session,
+                        start_date: datetime.datetime = datetime.datetime(2022, 8, 30, 9, 30),
+                        end_date: datetime.datetime = datetime.datetime(2022, 8, 30, 9, 30)):
+    return db.query(models.Flight).filter(and_(models.Flight.dag_utc_time_str >= start_date,
+                                               end_date >= models.Flight.dag_utc_time_str)).all()
+
